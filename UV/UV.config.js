a3fc95533daa52661dd6899d123e5a46a58f0408
DevEchoX2 <DevEchoX@gmail.com>
2025-07-02 11:50:27 -0400

    Update UV.config.js

--- UV/UV.config.js
+++ UV/UV.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
 	prefix: "/active/go/",
-	bare: "https:vplaza.org/bare
+	bare: "https://vplaza.org/bare
 	encodeUrl: Ultraviolet.codec.xor.encode,
 	decodeUrl: Ultraviolet.codec.xor.decode,
 	handler: "/static/uv/uv.handler.js",
